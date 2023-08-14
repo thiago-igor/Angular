53dfa4a5c2bd55657f16aec500384c24c8f7f0fc
thiago-igor <thiago.maia0606@gmail.com>
2023-08-14 09:55:55 -0300

    Atualização
    Introdução ao TypeScrypt

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// Tipos Básicos
+let age:number = 5
+const fristName: string = 'Felipe'
+const isValid: boolean = true
+let idk: any = 5
+
+const ids: number[] = [1,2,3,4,5]
+const boolean: boolean[] = [true,false,true]
+const names: string[] = ['Felipe', 'Andre']
+
+console.log(age);
+
+
+//TUPULAS
+
+const person: [number, string] = [50, 'Andre'] //tem que por na ordem predeterminada
+
+const people: [number, string] [] =  
+[[50, 'Andre'],
+[45, 'Thiago'],
+[24, 'Sabrina'],
+[30, 'Leo']]
+
+
+
+//INTERSECTION:
+
+const productId: string | number | boolean = false // a variavel pode ser qualquer das opçoes, nesse caso "false"
+
+
+
+//Enum:
+
+
+
+
